bfe107041ff8757318ef731f55891706ed4ed517
zero.li <zeroliql@gmail.com>
2023-08-22 06:09:41 -0700

    add expression parser

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -49,10 +49,13 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // for now, just print the tokens
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -64,5 +67,13 @@ public class Lox {
         System.err.println(
             "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+        report(token.line, " at end", message);
+        } else {
+        report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
