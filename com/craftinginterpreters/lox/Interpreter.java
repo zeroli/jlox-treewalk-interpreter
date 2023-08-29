17f11ded7c1cb76869aae17775a2ece649f1edff
zero.li <zeroliql@gmail.com>
2023-08-29 07:08:25 -0700

    support logical and/or operators

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -23,6 +23,18 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {  // and operator
+            if (!isTruthy(left)) return left;
+        }
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -40,7 +40,11 @@ class Parser {
      * expression  -> assignment
      *                  ;
      * assignment   -> IDENTIFIER "=" assignment
-     *                      | equality
+     *                      | logic_or
+     *                      ;
+     * logic_or         -> logic_and ( "or" logic_and ) *
+     *                      ;
+     * logic_and        -> equality ( "and" equality )*
      *                      ;
      * equality         -> comparison (("==" | "!=") comparison)*
      *                      ;
@@ -140,7 +144,7 @@ class Parser {
     }
 
     private Expr assignment() {
-        Expr expr = equality();
+        Expr expr = or();
 
         if (match(EQUAL)) {
             Token equals = previous();
@@ -152,6 +156,30 @@ class Parser {
             }
             error(equals, "Invalid assignment target.");
         }
+        return expr;
+    }
+
+    private Expr or() {
+        Expr expr = and();
+
+        while (match(OR)) {
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr and() {
+        Expr expr = equality();
+
+        while (match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
         return expr;
     }
 
